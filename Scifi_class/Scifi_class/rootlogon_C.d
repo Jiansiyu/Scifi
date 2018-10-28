b1ff6a8bb3a4e796d5dc897209eedf257024ad1a
John Williamson <jwilli@ifarm1401.jlab.org>
2018-10-27 23:21:49 -0400

    rootlogon dependency file added

---
+++ Scifi_class/Scifi_class/rootlogon_C.d
@@ -0,0 +1,4 @@
+
+# DO NOT DELETE
+./Scifi_class/rootlogon_C.so: /u/apps/root/6.12.06/root/include/RVersion.h /u/apps/root/6.12.06/root/include/RConfig.h /u/apps/root/6.12.06/root/include/TClass.h /u/apps/root/6.12.06/root/include/TDictAttributeMap.h /u/apps/root/6.12.06/root/include/TInterpreter.h /u/apps/root/6.12.06/root/include/TROOT.h /u/apps/root/6.12.06/root/include/TBuffer.h /u/apps/root/6.12.06/root/include/TMemberInspector.h /u/apps/root/6.12.06/root/include/TError.h /u/apps/root/6.12.06/root/include/RtypesImp.h /u/apps/root/6.12.06/root/include/TIsAProxy.h /u/apps/root/6.12.06/root/include/TFileMergeInfo.h /u/apps/root/6.12.06/root/include/TCollectionProxyInfo.h /u/apps/root/6.12.06/root/bin/rootcling
+rootlogon_C__ROOTBUILDVERSION= 6.12/06
