3407c436d0a8724b1f482d8b1f8ecd6a72ff961d
John Williamson <jwilli@buckfast.jlab.org>
2018-11-26 14:33:42 -0500

    added items to .gitignore, also a test of new git ignore alias used to do this

---
+++ scifitest_Oct2018/JW2_copy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+ run=5056
+
+
+
+#for run in {5006..5048}
+#do
+#scp wm122:~/FADC_${run}.dat data/
+#scp eel122@hapc4:~/data2/SciFi_${run}.dat data/
+
+#analyzer "replay_hcal_test.C(${run},100000)" <<EOF
+analyzer "replay_hcal_test.C(${run})" <<EOF
+.x conv2.cc(${run},0)
+.x conv2.cc(${run},1)
+.q
+EOF
+
+
+#done
+#root -l<<EOF
+#.x 
+#EOF
+
+# .x conv1_JW.cc(${run},0)
+# .x conv1_JW.cc(${run},1)
